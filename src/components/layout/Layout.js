b00404f63c7fcd28458427202b62cd0296af6ee4
Cem GOKCE <cemgokce9619@gmail.com>
2022-01-10 13:48:16 +0300

    Quotes project complited.

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import classes from './Layout.module.css'
+import MainNavigation from './MainNavigation';
+import React from 'react'
+
+const Layout = (props) => {
+    return (
+        <React.Fragment>
+            <MainNavigation />
+            <main className={classes.main}>{props.children}</main>
+        </React.Fragment>
+    )
+}
+export default Layout
---
+++ src/pages/NewQuote.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import QuoteForm from '../components/quotes/QuoteForm'
+import useHttp from '../hooks/hooks/use-http'
+import { addQuote } from '../lib/lib//api'
+
+const NewAllQuote = () => {
+    const { sendRequest, status } = useHttp(addQuote);
+    const history = useHistory();
+
+    useEffect(() => {
+
+        if (status === 'completed') {
+            history.push('/quotes');
+        }
+
+    }, [status, history])
+
+
+    const addQuoteHandler = quoteData => {
+
+        sendRequest(quoteData)
+    }
+
+    return (
+        <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+    )
+}
+
+export default NewAllQuote
